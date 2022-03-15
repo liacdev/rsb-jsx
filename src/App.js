d3ab1ac0aac9b78adf301ddeacfcdf03968b9525
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-03-15 13:41:58 -0500

    functional components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import JSX from "./Components/jsx.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>React JSX</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/SouthpawCurveConcepts/rsb-jsx"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to repo
         </a>
+        <JSX />
       </header>
     </div>
   );
---
+++ src/Components/jsx.js
@@ -0,0 +1,13 @@
+function JSX() {
+  return (
+    <div>
+      <h2>React JSX</h2>
+      <p>
+        JSX stands for JavaScript XML. JSX allows us to write HTML in React. JSX
+        makes it easier to write and add HTML in React.
+      </p>
+    </div>
+  );
+}
+
+export default JSX;
